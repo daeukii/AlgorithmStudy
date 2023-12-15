7704588ae89e3701c3e042199e862140e313fe4b
daeukii <gjeodnr2@gmail.com>
2023-12-15 16:03:12 +0900

    231215

--- src/W3/W3_4.java
+++ src/W3/W3_4.java
@@ -7,16 +7,23 @@ public class W3_4 {
     public static void main(String[] args) {
         // 백준 10871번 https://www.acmicpc.net/problem/10871
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int X = scanner.nextInt();
 
-        ArrayList<Integer> intList = new ArrayList<Integer>();
-        for(int i=0; i<N; i++){
-            int num = scanner.nextInt();
-            if(num < X)
-                intList.add(num);
+        int N, X;
+
+        N = scanner.nextInt();
+        X = scanner.nextInt();
+
+        // 정수 N 개로 이루어진 수열 A
+        int A[] = new int[N];
+
+        for(int i=0; i<N; i++) {
+            A[i] = scanner.nextInt();
+
+            // 수열 A 에서 X 보다 작은 수를 모두 출력
+            if(A[i] < X) {
+                System.out.println(A[i]);
+            }
         }
-        for(int i: intList)
-            System.out.println(i);
+
     }
 }
