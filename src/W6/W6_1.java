000ba8c3233729c0ef49237ded3fa85f5667fe0b
daeukii <gjeodnr2@gmail.com>
2024-01-05 17:35:28 +0900

    240105

--- src/W6/W6_1.java
+++ src/W6/W6_1.java
@@ -6,30 +6,21 @@ import java.io.InputStreamReader;
 
 public class W6_1 {
     public static void main(String[] args) throws IOException {
-        // 백준 2745번 https://www.acmicpc.net/problem/2745
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+        // 백준 2903번 https://www.acmicpc.net/problem/2903
 
-        char[] N = input[0].toCharArray();  // 숫자를 입력받아 char 배열로
-        int B = Integer.parseInt(input[1]);  // 무슨 진법인지
-        int result = 0;  // 계산 결과 저장 변수
+        // 패턴을 우선 보면 모든 점의 수는 한변의 점의 제곱이다.
+        // 점의 갯수는 늘어날때마다 현재 점의 -1만큼 늘어난다.
+        // 처음에 2개였던 점이 다음에 2+(2-1)가 되어서 3이 되고,
+        // 다음엔 3+(3-1) = 5, 또 다음엔 5+(5-1) = 9 이렇게 늘어난다.
+        // 그렇다면 반복문으로 입력값만큼 한 변의 점의 갯수를 늘리고 그 갯수를 제곱해서 출력한다.
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            int d = 2;
 
-        // 글자를 배열의 인덱스 0부터 꺼내오는데 쓸 변수 i
-        // 그리고 (글자의길이-1)부터 시작하는 자릿수 변수 exp(exp는 지수를 뜻한다)
-        for(int i=0, exp=N.length-1; i<N.length; i++, exp--){
-            int asc = (int) N[i];  // 현재 글자의 아스키코드값
-
-            if('0' <= asc && asc <= '9'){  // 만약 현재 글자가 0-9이면(즉 일반 숫자이면)
-                asc -= 48;  // 0의 아스키코드 값인 48을 뺀다
-            }else{
-                asc -= 55;  // 아니면 알파벳 대문자이다. A가 10이어야 하므로 55를 빼준다
+            for(int i = 0; i < N; i++) {
+                d += (d - 1);
             }
-
-            result += (Math.pow(B, exp) * asc);  // 위의 진법 변환 공식을 그대로 구현한 것
+            System.out.println(d * d);
         }
-
-        System.out.println(result);
-        br.close();
-    }
 }
